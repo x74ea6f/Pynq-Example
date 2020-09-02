021f7eaf1829736efa97c71894005c80eade11d5
x74ea6f <70569142+x74ea6f@users.noreply.github.com>
2020-09-02 16:44:19 +0900

    add vivado

---
+++ Pynq_test_hls/pynq_vivado/pynq_vivado.srcs/sources_1/bd/top_design/hdl/top_design_wrapper.v
@@ -0,0 +1,145 @@
+//Copyright 1986-2020 Xilinx, Inc. All Rights Reserved.
+//--------------------------------------------------------------------------------
+//Tool Version: Vivado v.2020.1 (win64) Build 2902540 Wed May 27 19:54:49 MDT 2020
+//Date        : Wed Sep  2 15:56:37 2020
+//Host        : DESKTOP-OMB4GFF running 64-bit major release  (build 9200)
+//Command     : generate_target top_design_wrapper.bd
+//Design      : top_design_wrapper
+//Purpose     : IP block netlist
+//--------------------------------------------------------------------------------
+`timescale 1 ps / 1 ps
+
+module top_design_wrapper
+   (DDR_0_addr,
+    DDR_0_ba,
+    DDR_0_cas_n,
+    DDR_0_ck_n,
+    DDR_0_ck_p,
+    DDR_0_cke,
+    DDR_0_cs_n,
+    DDR_0_dm,
+    DDR_0_dq,
+    DDR_0_dqs_n,
+    DDR_0_dqs_p,
+    DDR_0_odt,
+    DDR_0_ras_n,
+    DDR_0_reset_n,
+    DDR_0_we_n,
+    FIXED_IO_0_ddr_vrn,
+    FIXED_IO_0_ddr_vrp,
+    FIXED_IO_0_mio,
+    FIXED_IO_0_ps_clk,
+    FIXED_IO_0_ps_porb,
+    FIXED_IO_0_ps_srstb,
+    led_b4,
+    led_tri_io);
+  inout [14:0]DDR_0_addr;
+  inout [2:0]DDR_0_ba;
+  inout DDR_0_cas_n;
+  inout DDR_0_ck_n;
+  inout DDR_0_ck_p;
+  inout DDR_0_cke;
+  inout DDR_0_cs_n;
+  inout [3:0]DDR_0_dm;
+  inout [31:0]DDR_0_dq;
+  inout [3:0]DDR_0_dqs_n;
+  inout [3:0]DDR_0_dqs_p;
+  inout DDR_0_odt;
+  inout DDR_0_ras_n;
+  inout DDR_0_reset_n;
+  inout DDR_0_we_n;
+  inout FIXED_IO_0_ddr_vrn;
+  inout FIXED_IO_0_ddr_vrp;
+  inout [53:0]FIXED_IO_0_mio;
+  inout FIXED_IO_0_ps_clk;
+  inout FIXED_IO_0_ps_porb;
+  inout FIXED_IO_0_ps_srstb;
+  output led_b4;
+  inout [3:0]led_tri_io;
+
+  wire [14:0]DDR_0_addr;
+  wire [2:0]DDR_0_ba;
+  wire DDR_0_cas_n;
+  wire DDR_0_ck_n;
+  wire DDR_0_ck_p;
+  wire DDR_0_cke;
+  wire DDR_0_cs_n;
+  wire [3:0]DDR_0_dm;
+  wire [31:0]DDR_0_dq;
+  wire [3:0]DDR_0_dqs_n;
+  wire [3:0]DDR_0_dqs_p;
+  wire DDR_0_odt;
+  wire DDR_0_ras_n;
+  wire DDR_0_reset_n;
+  wire DDR_0_we_n;
+  wire FIXED_IO_0_ddr_vrn;
+  wire FIXED_IO_0_ddr_vrp;
+  wire [53:0]FIXED_IO_0_mio;
+  wire FIXED_IO_0_ps_clk;
+  wire FIXED_IO_0_ps_porb;
+  wire FIXED_IO_0_ps_srstb;
+  wire led_b4;
+  wire [0:0]led_tri_i_0;
+  wire [1:1]led_tri_i_1;
+  wire [2:2]led_tri_i_2;
+  wire [3:3]led_tri_i_3;
+  wire [0:0]led_tri_io_0;
+  wire [1:1]led_tri_io_1;
+  wire [2:2]led_tri_io_2;
+  wire [3:3]led_tri_io_3;
+  wire [0:0]led_tri_o_0;
+  wire [1:1]led_tri_o_1;
+  wire [2:2]led_tri_o_2;
+  wire [3:3]led_tri_o_3;
+  wire [0:0]led_tri_t_0;
+  wire [1:1]led_tri_t_1;
+  wire [2:2]led_tri_t_2;
+  wire [3:3]led_tri_t_3;
+
+  IOBUF led_tri_iobuf_0
+       (.I(led_tri_o_0),
+        .IO(led_tri_io[0]),
+        .O(led_tri_i_0),
+        .T(led_tri_t_0));
+  IOBUF led_tri_iobuf_1
+       (.I(led_tri_o_1),
+        .IO(led_tri_io[1]),
+        .O(led_tri_i_1),
+        .T(led_tri_t_1));
+  IOBUF led_tri_iobuf_2
+       (.I(led_tri_o_2),
+        .IO(led_tri_io[2]),
+        .O(led_tri_i_2),
+        .T(led_tri_t_2));
+  IOBUF led_tri_iobuf_3
+       (.I(led_tri_o_3),
+        .IO(led_tri_io[3]),
+        .O(led_tri_i_3),
+        .T(led_tri_t_3));
+  top_design top_design_i
+       (.DDR_0_addr(DDR_0_addr),
+        .DDR_0_ba(DDR_0_ba),
+        .DDR_0_cas_n(DDR_0_cas_n),
+        .DDR_0_ck_n(DDR_0_ck_n),
+        .DDR_0_ck_p(DDR_0_ck_p),
+        .DDR_0_cke(DDR_0_cke),
+        .DDR_0_cs_n(DDR_0_cs_n),
+        .DDR_0_dm(DDR_0_dm),
+        .DDR_0_dq(DDR_0_dq),
+        .DDR_0_dqs_n(DDR_0_dqs_n),
+        .DDR_0_dqs_p(DDR_0_dqs_p),
+        .DDR_0_odt(DDR_0_odt),
+        .DDR_0_ras_n(DDR_0_ras_n),
+        .DDR_0_reset_n(DDR_0_reset_n),
+        .DDR_0_we_n(DDR_0_we_n),
+        .FIXED_IO_0_ddr_vrn(FIXED_IO_0_ddr_vrn),
+        .FIXED_IO_0_ddr_vrp(FIXED_IO_0_ddr_vrp),
+        .FIXED_IO_0_mio(FIXED_IO_0_mio),
+        .FIXED_IO_0_ps_clk(FIXED_IO_0_ps_clk),
+        .FIXED_IO_0_ps_porb(FIXED_IO_0_ps_porb),
+        .FIXED_IO_0_ps_srstb(FIXED_IO_0_ps_srstb),
+        .led_b4(led_b4),
+        .led_tri_i({led_tri_i_3,led_tri_i_2,led_tri_i_1,led_tri_i_0}),
+        .led_tri_o({led_tri_o_3,led_tri_o_2,led_tri_o_1,led_tri_o_0}),
+        .led_tri_t({led_tri_t_3,led_tri_t_2,led_tri_t_1,led_tri_t_0}));
+endmodule
