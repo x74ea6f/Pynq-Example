a14f95fa379e3b3789e0d5d4465f265da4feb70e
x74ea6f <70569142+x74ea6f@users.noreply.github.com>
2020-09-25 17:09:47 +0900

    update

--- mnist/vivado/vivado.srcs/sources_1/bd/test_mnist/hdl/test_mnist_wrapper.v
+++ mnist/vivado/vivado.srcs/sources_1/bd/test_mnist/hdl/test_mnist_wrapper.v
@@ -1,7 +1,7 @@
 //Copyright 1986-2020 Xilinx, Inc. All Rights Reserved.
 //--------------------------------------------------------------------------------
 //Tool Version: Vivado v.2020.1 (win64) Build 2902540 Wed May 27 19:54:49 MDT 2020
-//Date        : Thu Sep 24 17:09:07 2020
+//Date        : Fri Sep 25 16:56:01 2020
 //Host        : DESKTOP-OMB4GFF running 64-bit major release  (build 9200)
 //Command     : generate_target test_mnist_wrapper.bd
 //Design      : test_mnist_wrapper
